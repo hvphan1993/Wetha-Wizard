5814492754f5509b2686301f59657089aaa7ee95
hvphan <hvphan1993@gmail.com>
2022-05-06 22:32:47 -0700

    added comments to stay focused, restructured api call- will add more after getting help

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,27 @@
-function findWeatherApiOne(a, b) {
-  var queryURL2 =
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-    a +
-    "&lon=" +
-    b +
-    "&exclude=minutely,hourly&appid=aec299195260a001b09706b5bfe740f7&units=imperial";
-  // second API call for rest of the current weather data + 5 day forecast
-  $.ajax({
-    url: queryURL2,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    // clears the 5- day forecast before displaying next set of city data
-    $(".card-deck").empty();
-    var icon = response.current.weather[0].icon;
-    var iconImg = $("<img>");
-    // append weather icon to the page
-    iconImg.addClass("img-fluid");
-    iconImg.attr(
-      "src",
-      "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-    );
+// need to have current weather status, five day forecast (connect to moment??) color icons for UV index, icons to show up, dynamically created button elements
+// buttons saved from local storage so user can search/click previous selections again if needed, color match cards and boxes to match example, gradient for top bar?
+// api calls- get help if needed
+
+$(document).ready(function () {
+  var cityName = "";
+  // latitude and longitude retrieved in API 1 to be used in second API call
+  var lat = "";
+  var lon = "";
+  // call to get the current weather and the daily weather
+  function findWeatherApiOne(a, b) {
+    var searchUrl2 =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      a +
+      "&lon=" +
+      b +
+      "&exclude=minutely,hourly&appid=aec299195260a001b09706b5bfe740f7&units=imperial";
+    // second API call for rest of the current weather data + 5 day forecast
+    $.ajax({
+      url: searchUrl2,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+
+    }
+  }
+}
